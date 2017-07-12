0f177b1afa1a6de0ea76d4566b6fb4563a6a22e4
Антон <scouturl@mail.ru>
2017-07-13 01:58:44 +0300

    index page with responsive adaptive typographic

---
+++ public/js/gulp-boilerplate.js
@@ -0,0 +1,13 @@
+(function() { 'use strict';
+
+var msg = 'gulper';
+var greet = function greet(txt) {
+  return 'Hello ' + txt;
+};
+greet(msg);
+"use strict";
+
+var sum = function sum(a, b) {
+  return a + b;
+};
+sum(1, 2); }());
